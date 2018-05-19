64e9e5a7f1bca7852d51c0fc5ccf929c21295fb6
Patrick Tsai <patrick@simplypatrick.com>
2018-05-18 23:59:29 -0700

    Manage player in ViewModel

--- app/src/main/java/com/simplypatrick/loveyourplayer/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/simplypatrick/loveyourplayer/ui/player/PlayerFragment.kt
@@ -1,16 +1,33 @@
 package com.simplypatrick.loveyourplayer.ui.player
 
+import android.Manifest
+import android.app.Activity
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.simplypatrick.loveyourplayer.R
-import com.simplypatrick.loveyourplayer.YourPlayer
 import kotlinx.android.synthetic.main.player_fragment.*
+
+fun Activity.askForPermission(permission: String, requestCode: Int): Boolean {
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            // show rationale
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        return false
+    }
+    return true
+}
 
 class PlayerFragment : Fragment() {
 
@@ -29,24 +46,29 @@ class PlayerFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PlayerViewModel::class.java)
 
-        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        if (activity?.askForPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 100) == true) {
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
-        surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun surfaceDestroyed(p0: SurfaceHolder?) {
-            }
-
-            override fun surfaceCreated(p0: SurfaceHolder?) {
-                YourPlayer().apply {
-                    Thread {
-                        init(surfaceView.holder.surface, "/sdcard/Movies/big_buck_bunny_480p_h264.mov")
-                        playbackLoop()
-                    }.start()
+            surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+                override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
                 }
-            }
-        })
+
+                override fun surfaceDestroyed(holder: SurfaceHolder) {
+                }
+
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    viewModel.init(holder.surface)
+                }
+            })
+
+            viewModel.error.observe(this, Observer {
+                Toast.makeText(activity, it, Toast.LENGTH_LONG).show()
+            })
+        }
     }
 
+    override fun onPause() {
+        super.onPause()
+        viewModel.stop()
+    }
 }
--- app/src/main/java/com/simplypatrick/loveyourplayer/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/simplypatrick/loveyourplayer/ui/player/PlayerViewModel.kt
@@ -1,7 +1,28 @@
 package com.simplypatrick.loveyourplayer.ui.player
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.view.Surface
+import com.simplypatrick.loveyourplayer.YourPlayer
 
 class PlayerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var error = MutableLiveData<String>()
+    var player: YourPlayer? = null
+
+    fun init(surface: Surface) {
+        player = YourPlayer().apply {
+            Thread {
+                val ok = init(surface, "/sdcard/Movies/big_buck_bunny_480p_h264.mov")
+                if (!ok) {
+                    error.postValue("Failed to play video")
+                }
+                playbackLoop()
+            }.start()
+        }
+    }
+
+    fun stop() {
+        player?.stop()
+        player = null
+    }
 }
